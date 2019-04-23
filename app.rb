59d47762c1a6386d0108a22b9031d943fde8cd9b
Patrick McCombs <pa.mccombs1@gmail.com>
2019-04-23 13:13:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,16 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/' do
-    puts "Welcome!"
     erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
+    erb :display_puppy
   end
 
 end
